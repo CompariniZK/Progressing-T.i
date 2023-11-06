d05a012eab756488229d69339e8cc973eef4ecc5
Gabriel <gabriel.comparini.ismart@gmail.com>
2023-11-06 17:28:49 +0100

    Array, primeiro programa

---
+++ Dia 10/index.js
@@ -0,0 +1,17 @@
+let arrayNumeros = []
+let pergunta = 0
+let loop = true
+let contador = 0
+
+while(loop === true){
+    
+   pergunta = Number(prompt("Insira um número inteiro e positivo (a cima de 0 ou 0."))
+   arrayNumeros [contador] = pergunta
+   contador++
+
+   if(contador === 5){
+    break;
+   }
+}
+
+console.log(arrayNumeros)
