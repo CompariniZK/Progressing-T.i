9c28fd2359e404bb1b722a54f307d423a43a2cb8
Gabriel <gabriel.comparini.ismart@gmail.com>
2023-11-19 20:21:07 +0100

    Programando um sistema com 2 objetos

---
+++ Dia 13 ObjetosC/Exercicio 2/gbr.js
@@ -0,0 +1,108 @@
+class CARROS
+{
+nome
+potencia
+velocidadeM
+aceleracao
+ constructor(nome, potencia , velocidadeM, aceleracao, distanciaP){
+
+    this.nome = nome
+    this.potencia = potencia
+    this.velocidadeM = velocidadeM
+    this.aceleracao = aceleracao
+    this.distanciaP = distanciaP
+
+function distancia()
+    {
+    let pouco = 0
+    let vencedor12 = []
+    let tempoSEG = this.distanciaP / (this.velocidadeM / this.aceleracao)
+    pouco++
+    vencedor12[pouco] = tempoSEG
+    
+    let sorted = vencedor12.sort((a,b) => { 
+
+    if(a > b) return 1;
+    if(a < b) return -1;
+    return 0;
+
+    })
+
+    return sorted;
+
+}
+
+ }
+
+
+}
+let whilezin = true
+let contador = 0
+let carrao = []
+let vencedor10 = 0
+let vencedor0 = 0
+
+while(whilezin)
+{
+let nomes = prompt("Insira o nome do carro:")
+let potencias = Number(prompt("Insira o número da potência do carro: "))
+let velocidadeMs = Number(prompt("Insira a velocidade máxima desse carro"))
+let aceleracaoZK = Number(prompt("Quantos segundos o carro leva para ir de 0 a 100?"))
+let distanciaPs = Number(prompt("Insira a distância a percorrer:"))
+let carrinho = new CARROS(nomes, potencias, velocidadeMs, aceleracaoZK , distanciaPs,)
+CARROS.distancia
+
+
+carrao[contador] = carrinho
+
+contador++
+
+let stopz = Number(prompt("pretende adicionar mais carros? (digite 2) pra não"))
+
+if(stopz === 2){
+
+whilezin = false;
+
+}
+
+}
+
+
+class CORRIDA
+{
+
+nome 
+tipo
+distancia
+participantes
+vencedor
+
+constructor(nome,tipo,distancia,participantes,vencedor)
+{
+this.nome = nome
+this.tipo = tipo
+this.distancia = distancia
+this.participantes = participantes
+this.vencedor = vencedor
+}
+theWinnerIs()
+{
+console.log("Os vencedores em ordem foram: " + sorted)
+return;
+}
+}
+
+let nomes3 = prompt("Insira o nome do local da corrida:")
+let tipos2 = prompt("Insira o tipo da corrida:")
+let distancia = Number(prompt("Trajeto total da corrida"))
+let participantes = carrao
+let vencedores = CARROS.distancia
+
+let corridinhas = new CORRIDA(nomes3, tipos2, distancia, participantes, vencedores)
+let corridaCD = [corridinhas]
+console.log(carrao)
+console.log(corridaCD)
+
+
+
+
