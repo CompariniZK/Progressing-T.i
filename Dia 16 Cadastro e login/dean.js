cb3a7c80cede1ab994c9ea7df228d3ef6f7f97f0
Gabriel <gabriel.comparini.ismart@gmail.com>
2023-11-29 12:49:45 +0100

    Arrumei a função login

--- Dia 16 Cadastro e login/dean.js
+++ Dia 16 Cadastro e login/dean.js
@@ -1,7 +1,11 @@
+let contador = 0
+let nomeCliente = []
+let senhaCliente = []
 demanda();
 
 function demanda(){
-
+let coisa = true   
+while(coisa = true){
 let pedido = parseInt(prompt("Escolha uma das opções: (1)Cadastro; (2) login; (3) exclusão de uma conta (4) Encerrar "))
 
 if(pedido === 1)
@@ -14,18 +18,20 @@ if(pedido === 2){
 }
 if(pedido === 3)
 {
-    exclusao();
+    exclusao(nome3);
 }
 
 
 
 }
+
+
 
 function cadastro(){
 
-let contador = 0
-let nomeCliente = []
-let senhaCliente = []
+let coisa2 = true
+
+while(coisa2 = true){
 
 let nome = prompt("Digite seu nome:")
 let senha = Number(prompt("Digite sua senha em números"))
@@ -34,40 +40,42 @@ nomeCliente[contador] = nome
 senhaCliente[contador] = senha
 
 contador++
-let sera = Number(prompt("Deseja fazer login? (1) para sim"))
+let acabou = Number(prompt("Deseja adicionar mais algum cadastro? (1) para SIM; (2) para NÃO"))
+if(acabou === 2 ){
+    coisa2 = false;
+let sera = Number(prompt("Deseja fazer login? (1) para sim; (2) para não"))
+
 if(sera === 1){
 
-    login();
-    let acabou = Number(prompt("Deseja adicionar mais algum cadastro? (1) para SIM; (2) para NÃO"))
-
-    if(acabou === 1){
+    login(nomeCliente, senhaCliente);
+   
+}
+    if(sera === 2){
  
-        cadastro();
+        demanda();
                     }
-          else{
-
-            demanda();
+         
 
 
           }          
 }
 
 
+}
 
 
-
-function login(nome2, senha2){
+function login(nomeCliente, senhaCliente){
 let loginzao = true
 
 
 while(loginzao = true){
- nome2 = prompt("Insira seu nome de login")
- senha2 = Number(prompt("Digite sua senha"))
+ let nome2 = prompt("Insira seu nome de login")
+ let senha2 = Number(prompt("Digite sua senha"))
  
- nomeCliente.includes(nome2)
- senhaCliente.includes(senha2)
+ nomeCliente.includes(nome2);
+ senhaCliente.includes(senha2);
 
- if(nomeCliente.includes(nome2) === true && senhaCliente.includes(senha2) === true)
+ if(nomeCliente.includes(nome2) == true && senhaCliente.includes(senha2) == true )
  {
 
 console.log("login Successful")
@@ -78,7 +86,7 @@ break
 }
 }
 
-function exclusao (nome3){
+function exclusao(nome3){
 
 nome3 = prompt("Insira o nome da conta que deseja excluir")
 let senha3 = parseInt(prompt("Insira a senha da conta que deseja exlcuir"))
@@ -97,6 +105,7 @@ senhaCliente.splice(senhaCliente.includes(senha3))
 
 }
 }
+
  let acabou2 = Number(prompt("Deseja adicionar mais? (1) para SIM; (2) para NÃO"))
 
 if(acabou2 === 1){
