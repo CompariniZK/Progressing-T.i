1800342103b516ecc891f324ec91f2a647aa262e
Gabriel <comparinizk@outlook.com>
2024-02-11 13:52:22 +0100

    Tabuadinha com For só para praticar

--- Practice JS mode/Practice.js
+++ Practice JS mode/Practice.js
@@ -1,11 +1,27 @@
+let numeroEntrada = Number(prompt("Digite um número até 10"))
+console.log("TABUADA DO " + numeroEntrada + " ABAIXO:")
+for(let contador = 0; contador <= 10; contador++ ){
 
+    let resultado = contador * numeroEntrada;
+    console.log(numeroEntrada + " x " + contador + " = " + resultado);
 
+}
 
-for(let contador = 10; contador >=0; contador--){
+let numeroEntrada2 = numeroEntrada + 1
+console.log("TABUADA DO " + numeroEntrada2 + " ABAIXO:")
 
-let multiplica = contador * 5;
+for(let contador2 = 0; contador2 <= 10; contador2++){
 
-console.log(multiplica);
+    let resultado2 = contador2 * numeroEntrada2;
+    console.log(numeroEntrada2 + " x " + contador2 + " = " + resultado2);
+}
 
+let numeroEntrada3 = numeroEntrada2 + 1
+console.log("TABUADA DO " + numeroEntrada3 + " ABAIXO:")
+
+for(let contador3 = 0; contador3 <= 10; contador3++){
+
+let resultado3 = contador3 * numeroEntrada3;
+console.log(numeroEntrada3 + " x " + contador3 + " = " + resultado3)
 
 }
