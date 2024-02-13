96a2505367d194585be9334d7e98db46627aabbf
Gabriel <comparinizk@outlook.com>
2024-02-13 16:31:12 +0100

    Adicionei contagem IMC

--- Practice JS mode/Practice.js
+++ Practice JS mode/Practice.js
@@ -17,3 +17,33 @@ if(idade >= 18){
 }
 
 console.log("Você nasceu há " + idade + " anos" + ", " + idadeEmMeses + " meses, " + idadeEmSemanas + " semanas e " + idadeEmDias + " dias.")
+
+let peso = Number(prompt("Digite o seu peso exato, apenas números:"));
+let altura = Number(prompt("Digite sua altura exata, apenas números:"));
+let imc = peso / (altura * altura);
+
+console.log("De acordo com os dados rescebidos, o seu IMC é de: " + imc);
+
+if(imc < 18.5){
+
+console.log("De acordo com seu imc, você está na faixa da magreza.")
+}
+
+if(imc >= 18.5 && imc < 24.9){
+
+    console.log("De acordo com seu imc, você está na faixa normal de peso.")
+
+}
+
+if(imc >= 24.9 && imc < 30){
+
+    console.log("De acordo com seu imc, você está na faixa de sobrepeso.")
+
+}
+
+if(imc >= 30 ){
+
+    console.log("De acordo com seu imc, você está na faixa da obesidade.")
+
+}
+
