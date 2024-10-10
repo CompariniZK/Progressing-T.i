fe72318869b704ccbfd5396e7710e0dea7fd62e5
Gabriel <comparinizk@outlook.com>
2024-10-10 18:32:20 +0200

    Restart day 02

--- Practice JS mode/Practice.js
+++ Practice JS mode/Practice.js
@@ -1,24 +1,54 @@
-let teste = parseInt(prompt("Digite um número para a tabuada: "));
+let nota = 0 
+let nome = 0
+let sexo = 0
+let continueS = 0
+let contadorM = 0
+let homensNaSala = [];
+let contadorF = 0
+let mulheresNaSala = [];
+let whilez = false;
 
-for(let contador = 0; contador <= 10; contador++){
+let question1 = Number(prompt("Você deseja cadastrar uma nota? 1 para sim e 2 para não."))
+    if(question1=== 1 ){
 
-  
-    console.log( teste + " x " + contador + " = " + teste*contador)
+        whilez = true;
+    }
+        
+let notasF = 0;
+let notasM = 0;
 
+    while(whilez === true){
 
+        nome = prompt("Digite o nome do aluno")
+        nota = Number(prompt("Entre com a nota do aluno:"))
+        sexo = prompt("Qual o sexo do aluno? M para masculino e F para feminino")
+            if(sexo == "M"){
+
+                contadorM++
+                homensNaSala[contadorM] = nota;
+                notasM += nota;
+            }
+
+            else{
+
+                contadorF++
+                if(nota > 7)
+                mulheresNaSala[contadorF] = nota;
+                notasF += nota;
+            }
+        continueS = parseInt(prompt("Deseja inserir a  nota de um novo aluno? 1 para sim e 2 para nao"))
+
+         if(continueS == 2){
+
+            let quantidadeGeral = contadorM + contadorF
+            let somaNotas = notasM + notasF
+            let mediaGeral = somaNotas / quantidadeGeral;
+
+            console.log("A média geral foi de " + mediaGeral + ";")
+            console.log("A quantidade de homens no boletim foi de: " + contadorM + " homens;")
+            console.log("A quantidade de mulheres acima de 7 foram de : " + mulheresNaSala)
+            
+            whilez = false;
 }
-console.log("Próximo é:")
-let teste2 = teste + 1; 
-for(contador = 0; contador <= 10; contador++ ){
-
-    console.log( teste2 + " x " + contador + " = " + teste2*contador )
-
-}
-console.log("Próximo é:")
-let teste3 = teste2 + 1;
-
-for(contador = 0; contador <= 10; contador++){
-
-    console.log(teste3 + " x " + contador + " = " + teste3*contador)
 
 }
