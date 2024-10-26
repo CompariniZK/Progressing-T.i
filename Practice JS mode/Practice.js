7c128654566c6f3675c5ff9784581124c5026fb1
Gabriel <comparinizk@outlook.com>
2024-10-26 11:29:58 +0200

    Classe A

--- Practice JS mode/Practice.js
+++ Practice JS mode/Practice.js
@@ -1,76 +1,45 @@
-let arrayNomes = [];
-let arraySenhas = [];
-let whilezin = true;
-let contador = 0;
-let nome = 0;
-let senha = 0;
+class Computer 
 
-while(whilezin === true){
+{
+    tipo
+    processamento
+    memoriaRam
+    armazenamento
 
-    let pergunta = Number(prompt("O que deseja fazer? 1 para cadastro, 2 para login e 3 para excluir cadastro"))
+    constructor(tipo, processamento, memoriaRam, armazenamento){
 
-    switch(pergunta){
+        this.tipo = tipo
+        this.processamento = processamento
+        this.memoriaRam = memoriaRam
+        this.armazenamento = armazenamento
 
-
-        case 1 :
-             nome = prompt("Digite seu nome:")
-            arrayNomes[contador] = nome
-
-             senha = prompt("Digite sua senha")
-            arraySenhas[contador] = senha
-
-            contador++
-            break;
-
-        case 2:
-
-             nome = prompt("Digite seu nome:")
-             senha = prompt("Digite sua senha")
-
-             for(let i = 0; i < arrayNomes.length; i++){
-
-                if(nome === arrayNomes[i] && senha === arraySenhas[i]){
-
-                    console.log("Logado mlk!")
-                    break;
-                } else{
-
-                    console.log("Acesso negado")
-                }
-             }
-
-             case 3:
-                
-                nome = prompt("Digite o nome:")
-                senha = prompt("Digite a senha")
-
-                let indexNomes = arrayNomes.indexOf(nome)
-                let indexSenhas = arraySenhas.indexOf(senha)
-
-            
-            if(indexNomes > -1 && indexSenhas > -1){
-
-                arrayNomes.splice(indexNomes, 1);
-                arraySenhas.splice(indexSenhas, 1);
-                console.log('Excluída com sucesso')
-
-            }
-            
-        
 
 
     }
 
 
-    let desejo = Number(prompt("Deseja fazer alguma outra ação? 1 para sim e 2 para não"))
+}
 
-        if(desejo === 2){
+let ssd = true;
+let contador = 0;
+let arrayPC = [];
+while(ssd === true){
 
+    let Tipo = prompt("Qual o tipo do seu pc")
+    let processador = prompt("Qual o seu processador")
+    let Ram = prompt("Qual a memoria Ram do seu pc?")
+    let Armazenamento = prompt("Quanto é o seu armazenamento?")
+      
+    const objetoPC = new Computer(Tipo , processador , Ram , Armazenamento)
+
+        arrayPC[contador] = objetoPC
+
+        let desejo = Number(prompt("Deseja sair? (1) para sim e 2 para nao"))
+
+        if(desejo === 1){
+
+            console.log(arrayPC)
             break;
         }
 
-
-
 }
-
-
