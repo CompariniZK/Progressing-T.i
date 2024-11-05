6fce24e83385a44d26efcebb3f32463de1d027d5
Gabriel <comparinizk@outlook.com>
2024-11-05 19:51:17 +0100

    Ok

--- Practice JS mode/Practice.js
+++ Practice JS mode/Practice.js
@@ -1,67 +1,11 @@
+let arrayA = [13, 20 , 22, 10, 24 ,20, 27, 17, 19, 14 ];
+let arrayB = [24, 22, 23, 11, 15, 17, 29, 14, 16, 21];
 
-let = salarioAumentado = 0 
-let aumentoQtd = 0;
+function chekin (arrayA, arrayB){
 
-function Empregado(){
+    let arrayComum = [];
 
-let nome = prompt("Qual o seu nome?")
-let salario= Number(prompt("Qual o teu salário?"))
+    arrayComum = arrayA.filter(comum => arrayB.includes(comum));
 
-CalculoAumento(salario, nome)
-
-
-
+    return  console.log("Os números em comuns dos arrays A e B são: "+ arrayComum)
 }
-
-
-function CalculoAumento(salario, nome){
-
-    if(salario <= 1500){
-
-    aumentoQtd = salario * (20/100)
-    salarioAumentado = salario + aumentoQtd
-        
-    ShowThen(salario, nome, aumentoQtd, salarioAumentado)
-
-
-    }
-
-    if(salario <= 2000 && salario > 1500 ){
-
-        aumentoQtd = salario * (15/100)
-        salarioAumentado = salario + aumentoQtd
-        
-        ShowThen(salario, nome, aumentoQtd, salarioAumentado);
-
-    }
-
-    if(salario <= 3000 && salario > 2000){
-
-        aumentoQtd = salario * (10/100)
-        salarioAumentado = salario + aumentoQtd
-        
-        ShowThen(salario, nome, aumentoQtd, salarioAumentado);
-    }
-
-    if(salario > 3000 ){
-
-        aumentoQtd = salario * (5/100)
-        salarioAumentado = salario + aumentoQtd
-       
-        ShowThen(salario, nome, aumentoQtd, salarioAumentado);
-    }
-
-}
-
-function ShowThen(salario, nome, aumentoQtd, salarioAumentado){
-
-console.log("O salário de " + nome +  " saiu de " + salario + " para " + salarioAumentado + " com um aumento de " + aumentoQtd + " euros!!!")
-    let desejo = Number(prompt("Você deseja colocar um novo empregado? 1 para sim e 2 para nao"))
-    
-    if(desejo === 1){
-        Empregado();
-    }
-}
-
-
-Empregado()
